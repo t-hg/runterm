19d136d6097ec598db756bd32b10a9430a6d6da5
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-09-15 11:22:30 +0200

    scrolling

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"strings"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/t-hg/oneshot-term/must"
+	"github.com/t-hg/oneshot-term/rlex"
 )
 
 const (
@@ -22,23 +24,65 @@ const (
 //go:embed assets
 var Assets embed.FS
 
+func maxLineSkip(text string) float32 {
+	lines := strings.Split(text, "\n")
+	maxLineSkip := float32(len(lines))
+	return maxLineSkip + float32(len(strings.Join(lines, "")))/(WinWidth-Padding) + WinHeight/(2*CharHeight)
+}
+
 func main() {
 	rl.InitWindow(WinWidth, WinHeight, "OneShot-Term")
 	fontData := must.Do2(Assets.ReadFile("assets/iosevka-regular.ttf"))
 	font := rl.LoadFontFromMemory(".ttf", fontData, int32(len(fontData)), FontSize, nil, 0)
 	text := string(must.Do2(Assets.ReadFile("assets/lorem.txt")))
+	var lineSkip float32 = 0
 	for !rl.WindowShouldClose() {
+    maxLineSkip := maxLineSkip(text)
+    lineSkip -= rl.GetMouseWheelMove();
+    if lineSkip < 0 {
+      lineSkip = 0
+    } else if lineSkip > maxLineSkip {
+      lineSkip = maxLineSkip
+    }
+		if rl.IsKeyPressed(rlex.KEY_DOWN) {
+			lineSkip++
+			if lineSkip > maxLineSkip {
+				lineSkip--
+			}
+		} else if rl.IsKeyPressed(rlex.KEY_UP) {
+			lineSkip--
+			if lineSkip < 0 {
+				lineSkip = 0
+			}
+		}
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.GetColor(ColorBg))
 		var x float32 = Padding
 		var y float32 = Padding
-		for _, char := range text {
-			if x > WinWidth-CharWidth-Padding || char == '\n' {
+		skip := lineSkip
+		cursor := 0
+		for cursor < len(text) {
+			char := text[cursor]
+			if x+CharWidth >= WinWidth-Padding || char == '\n' {
+				skip--
 				x = Padding
-				y += CharHeight
+				if skip < 0 {
+					skip = 0
+					y += CharHeight
+				}
+				if char == '\n' {
+					cursor++
+				}
 			} else {
-				rl.DrawTextEx(font, string(char), rl.Vector2{X: x, Y: y}, FontSize, FontSpacing, rl.GetColor(ColorFg))
+				if skip == 0 {
+          if char == ' ' && x == Padding {
+            // skip
+          } else {
+					  rl.DrawTextEx(font, string(char), rl.Vector2{X: x, Y: y}, FontSize, FontSpacing, rl.GetColor(ColorFg))
+          }
+				}
 				x += CharWidth
+				cursor++
 			}
 		}
 		rl.EndDrawing()
